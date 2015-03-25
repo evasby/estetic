884a758a463f5cae2ddc2222311c7055e39c6682
Vladimir Dmitriev <defurator@mail.ru>
2015-03-25 13:18:06 +0300

    begin

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Estetic</title>
+  <link rel="stylesheet" type="text/css" href="css/all.css">
+  <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+  <!--[if lt IE 9]>
+    <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+  <!--[if lte IE 8]>
+    <link rel="stylesheet" href="css/all-old-ie.css">
+  <![endif]-->
+  <!--[if gt IE 8]>
+    <link rel="stylesheet" href="css/all.css">
+  <![endif]-->
+  <!--[if lt IE 10]> 
+  <script type="text/javascript" src="js/jquery.placeholder.js"></script>
+  <![endif]-->
+</head>
+<body>
+  <div class="inner">
+    <section class="main">
+      <div class="main_root">
+        <div class="main_inner">
+          <a href="/" class="main_logo">Estetic</a>
+        </div>
+        <nav class="menu">
+          <ul class="menu_list">
+            <li class="menu_item">
+              <a href="" class="menu_link menu_link__active">Мебель</a>
+              <ul class="menu_list menu_list__l2">
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Комоды</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Тумбочки</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Столы</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Кровати</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Стулья</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2">Табурет</a>
+                </li>
+                <li class="menu_item menu_item__l2">
+                  <a href="" class="menu_link menu_link__l2 menu_link__l2__active">Офисная мебель</a>
+                </li>
+              </ul>
+            </li>
+            <li class="menu_item">
+              <a href="" class="menu_link">Освещение</a>
+            </li>
+            <li class="menu_item">
+              <a href="" class="menu_link">О нас</a>
+            </li>
+            <li class="menu_item">
+              <a href="" class="menu_link">Блог</a>
+            </li>
+            <li class="menu_item">
+              <a href="" class="menu_link">Контакты</a>
+            </li>
+          </ul>
+        </nav>
+        <div class="main_froot"></div>
+      </div>
+      <div class="main_footer">
+        <div class="main_phone">
+          (029) 764-38-54
+        </div>
+        <br>
+        <div class="main_email">
+          info@estetic.by
+        </div>
+        <div class="main_social">
+          <a href="/" class="main_vk">VK</a>
+          <a href="/" class="main_fb">FB</a>
+          <a href="/" class="main_ok">OK</a>
+        </div>
+      </div>
+    </section>
+    <section class="content">
+      
+    </section>
+  </div>
+</body>
+</html>
